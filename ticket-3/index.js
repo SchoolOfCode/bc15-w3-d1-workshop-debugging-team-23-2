44b61cdb76db3a889f2628219c959a9b8a9afd9f
James Robertson <jaarobertson92@gmail.com>
2023-09-18 15:30:48 +0100

    Bug Fix - converted number to string

--- ticket-3/index.js
+++ ticket-3/index.js
@@ -1,12 +1,17 @@
 function countNumberOfNinesInNumber(number) {
   let count = 0;
+  number = number.toString();
+}
 
-  for (let i = 0; i < number.length; i++) {
-    const digit = number[i];
-    if ("9" === digit) {
-      count++;
+  function countNumberOfNinesInNumber(number) {
+    let count = 0;
+    for (let i = 0; i < String(number).length; i++) {
+      const digit = String(number)[i];
+      if (9 == digit) {
+        count++;
+      }
     }
+    return count;
   }
 
-  return count;
-}
+console.log(countNumberOfNinesInNumber(9875749))
